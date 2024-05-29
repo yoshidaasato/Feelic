82f84d563d403d5a07e8b4e9df365144e1e6e819
yoshidaasato <akkunn1026@gmail.com>
2024-05-29 13:12:25 +0900

    gui update

--- main-gui.py
+++ main-gui.py
@@ -11,57 +11,68 @@ with open(file_path, encoding='utf_8') as f:
 
 i = 1
 listresalt = []
+endcount = 0
+
 #関数resaltを定義
-def resalt():
-    #変数wordentから値を取得
-    global wordent
-    searchword = wordent.get()
-    searchwindow.destroy()
-    #csvの読み込み
-    with open(file_path,encoding='utf_8') as file:
-        reader = csv.reader(file)
-        #各レコードの呼び出し
-        for row in reader:
-            #リストから検索(部分一致)
-            index = ','.join(row)
-            findcount = index.find(searchword)
-            global i
-            #find関数の返り値が0より大きいとき
-            if findcount >= 0:
-                findcount = -1
-                listresalt.append(row)
-                continue
-            #読み込んでいるレコードとレコード数が一致するとき
-            if i == len(lines):
-                    errorwindow = tk.Tk()
-                    errorwindow.title('error')
-                    errorwindow.geometry(f"{200}x{200}")
-                    errorlab = tk.Label(errorwindow,text='存在しません')
-                    errorlab.pack()
-                    errorwindow.mainloop()
-                    break
-            #find関数の返り値がdefaultのとき(-1)
-            else:
-                i = i + 1
-                continue
-    #結果を表示するウィンドウを生成
-    resaltwindow = tk.Tk()
-    resaltwindow.title('結果')
-    resaltlab = tk.Label(resaltwindow,text=listresalt)
-    resaltlab.pack()
-    resaltwindow.geometry(f"{200}x{200}")
-    resaltwindow.mainloop()
+while endcount == 0:
+    def end():
+         global endcount
+         endcount = endcount+1
+         searchwindow.destroy()
+    def resalt():
+        #変数wordentから値を取得
+        global wordent
+        searchword = wordent.get()
+        searchwindow.destroy()
+        #csvの読み込み
+        with open(file_path,encoding='utf_8') as file:
+            reader = csv.reader(file)
+            #各レコードの呼び出し
+            for row in reader:
+                #リストから検索(部分一致)
+                index = ','.join(row)
+                findcount = index.find(searchword)
+                global i
+                #find関数の返り値が0より大きいとき
+                if findcount >= 0:
+                    findcount = -1
+                    listresalt.append(row)
+                    continue
+                #読み込んでいるレコードとレコード数が一致するとき
+                if i == len(lines):
+                        errorwindow = tk.Tk()
+                        errorwindow.title('error')
+                        errorwindow.geometry(f"{200}x{200}")
+                        errorlab = tk.Label(errorwindow,text='存在しません')
+                        errorlab.pack()
+                        retrybtn = tk.Button(errorwindow,text='戻る',command=errorwindow.destroy)
+                        retrybtn.pack()
+                        errorwindow.mainloop()
+                        break
+                #find関数の返り値がdefaultのとき(-1)
+                else:
+                    i = i + 1
+                    continue
+        #結果を表示するウィンドウを生成
+        resaltwindow = tk.Tk()
+        resaltwindow.title('結果')
+        resaltlab = tk.Label(resaltwindow,text=listresalt)
+        resaltlab.pack()
+        resaltwindow.geometry(f"{200}x{200}")
+        resaltwindow.mainloop()
 
-#スタートのウィンドウ生成
-searchwindow = tk.Tk()
-searchwindow.title('検索')
-searchwindow.geometry(f"{200}x{200}")
-wordlab = tk.Label(searchwindow,text='検索ワード入力')
-wordlab.pack()
+    #スタートのウィンドウ生成
+    searchwindow = tk.Tk()
+    searchwindow.title('検索')
+    searchwindow.geometry(f"{200}x{200}")
+    wordlab = tk.Label(searchwindow,text='検索ワード入力')
+    wordlab.pack()
 
-wordent = tk.Entry(searchwindow,width=50)
-wordent.pack()
-searchbtn = tk.Button(searchwindow,text='検索',command=resalt)
-searchbtn.pack()
+    wordent = tk.Entry(searchwindow,width=50)
+    wordent.pack()
+    searchbtn = tk.Button(searchwindow,text='検索',command=resalt)
+    searchbtn.pack()
+    endbtn = tk.Button(searchwindow,text='終了',command=end)
+    endbtn.pack()
 
-searchwindow.mainloop()
+    searchwindow.mainloop()
