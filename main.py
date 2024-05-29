b2fcb3b58c978e2597caf9305acd8151eaf00356
yoshidaasato <168070299+yoshidaasato@users.noreply.github.com>
2024-05-29 11:30:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 #cording:utf_8
 import csv
-search = input('入力')
+
+file_path = 'sample.csv'  # CSVファイルのパスを指定してください
+
+searchword = input('入力')
 i = 1
 resalt = []
-file_path = 'sample.csv'  # CSVファイルのパスを指定してください
+#csvの読み込みとレコード数の読み取り
 with open(file_path, encoding='utf_8') as f:
     reader = csv.reader(f)
     lines = f.readlines()
-
+#csvの読み込み
 with open(file_path,encoding='utf_8') as f:
-    reader = csv.reader(f)   
+    reader = csv.reader(f)
+    #各レコードを呼び出し
     for row in reader:
+        #リストから検索(部分一致)
         index = ','.join(row)
-        count = index.find(search)
-        if count >= 0:
-            count = -1
+        findcount = index.find(searchword)
+        #find関数の返り値が0より大きいとき
+        if findcount >= 0:
+            findcount = -1
             resalt.append(row)
             continue
+        #読み込んでいるレコードとレコード数が一致するとき
         elif len(lines) == i:
             print('存在しません')
             break
+        #find関数の返り値がdefaultのとき(-1)
         else:
             i = i + 1
             continue
