b24b3b24d68dbe6cb3483e796a2d14c2c3a75223
yoshidaasato <168070299+yoshidaasato@users.noreply.github.com>
2024-05-29 11:29:44 +0900

    Update main-gui.py

--- main-gui.py
+++ main-gui.py
@@ -4,27 +4,34 @@ import tkinter as tk
 # CSVファイルを開く
 
 file_path = 'sample.csv'  # CSVファイルのパスを指定してください
+#csvの読み込みとレコード数の読み取り
 with open(file_path, encoding='utf_8') as f:
     reader = csv.reader(f)
     lines = f.readlines()
 
 i = 1
 listresalt = []
+#関数resaltを定義
 def resalt():
+    #変数wordentから値を取得
     global wordent
     searchword = wordent.get()
-    print(searchword)
-    makewindow.destroy()
-    with open(file_path,encoding='utf_8') as f:
-        reader = csv.reader(f)
+    searchwindow.destroy()
+    #csvの読み込み
+    with open(file_path,encoding='utf_8') as file:
+        reader = csv.reader(file)
+        #各レコードの呼び出し
         for row in reader:
+            #リストから検索(部分一致)
             index = ','.join(row)
-            count = index.find(searchword)
+            findcount = index.find(searchword)
             global i
-            if count >= 0:
-                count = -1
+            #find関数の返り値が0より大きいとき
+            if findcount >= 0:
+                findcount = -1
                 listresalt.append(row)
                 continue
+            #読み込んでいるレコードとレコード数が一致するとき
             if i == len(lines):
                     errorwindow = tk.Tk()
                     errorwindow.title('error')
@@ -33,9 +40,11 @@ def resalt():
                     errorlab.pack()
                     errorwindow.mainloop()
                     break
+            #find関数の返り値がdefaultのとき(-1)
             else:
                 i = i + 1
                 continue
+    #結果を表示するウィンドウを生成
     resaltwindow = tk.Tk()
     resaltwindow.title('結果')
     resaltlab = tk.Label(resaltwindow,text=listresalt)
@@ -43,16 +52,16 @@ def resalt():
     resaltwindow.geometry(f"{200}x{200}")
     resaltwindow.mainloop()
 
-
-makewindow = tk.Tk()
-makewindow.title('検索')
-makewindow.geometry(f"{200}x{200}")
-wordlab = tk.Label(makewindow,text='検索ワード入力')
+#スタートのウィンドウ生成
+searchwindow = tk.Tk()
+searchwindow.title('検索')
+searchwindow.geometry(f"{200}x{200}")
+wordlab = tk.Label(searchwindow,text='検索ワード入力')
 wordlab.pack()
 
-wordent = tk.Entry(makewindow,width=50)
+wordent = tk.Entry(searchwindow,width=50)
 wordent.pack()
-searchbtn = tk.Button(makewindow,text='検索',command=resalt)
+searchbtn = tk.Button(searchwindow,text='検索',command=resalt)
 searchbtn.pack()
 
-makewindow.mainloop()
+searchwindow.mainloop()
